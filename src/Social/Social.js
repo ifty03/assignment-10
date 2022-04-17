c67b5842d02f78fff4a56daea8022815d52708ab
ifty03 <ashikulislamb84@gmail.com>
2022-04-17 18:20:11 +0600

    social media all functionality added

--- src/Social/Social.js
+++ src/Social/Social.js
@@ -2,14 +2,33 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
+import auth from "../firebase.init";
+import {
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 
 const Social = () => {
+  /* all provider */
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+  const [signInWithFacebook] = useSignInWithFacebook(auth);
+  const [signInWithGithub] = useSignInWithGithub(auth);
   return (
     <div>
       <div className="flex justify-center text-2xl items-center mt-3 ">
-        <BsFacebook className="mx-3 text-blue-600 cursor-pointer"></BsFacebook>
-        <FcGoogle className="mx-3 text-3xl cursor-pointer"></FcGoogle>
-        <FaGithub className="mx-3 cursor-pointer"></FaGithub>
+        <BsFacebook
+          onClick={() => signInWithFacebook()}
+          className="mx-3 text-blue-600 cursor-pointer"
+        ></BsFacebook>
+        <FcGoogle
+          onClick={() => signInWithGoogle()}
+          className="mx-3 text-3xl cursor-pointer"
+        ></FcGoogle>
+        <FaGithub
+          onClick={() => signInWithGithub()}
+          className="mx-3 cursor-pointer"
+        ></FaGithub>
       </div>
     </div>
   );
